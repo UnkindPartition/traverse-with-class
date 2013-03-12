aef5b5ef1e97a4a1855a927ce5ff13d9602e4065
Roman Cheplyaka <roma@ro-che.info>
2013-03-12 13:03:52 +0200

    Pass proxy into functions; remove (ctx a) as GTraversable's superclass

--- Data/Generics/Traversable.hs
+++ Data/Generics/Traversable.hs
@@ -7,45 +7,45 @@ import Data.Monoid
 import Data.Functor.Identity
 import Data.Functor.Constant
 
-class ctx a => GTraversable (ctx :: * -> Constraint) a where
+class GTraversable (ctx :: * -> Constraint) a where
   gtraverse
     :: Applicative f
     => proxy ctx
-    -> (forall d . GTraversable ctx d => d -> f d)
+    -> (forall d . (GTraversable ctx d, ctx d) => proxy ctx -> d -> f d)
     -> a -> f a
 
 gmap
   :: GTraversable ctx a
   => proxy ctx
-  -> (forall d . GTraversable ctx d => d -> d)
+  -> (forall d . (GTraversable ctx d, ctx d) => proxy ctx -> d -> d)
   -> a -> a
-gmap ctx f = runIdentity . gtraverse ctx (Identity . f)
+gmap ctx f = runIdentity . gtraverse ctx (\ctx -> Identity . f ctx)
 
 gmapM
   :: (Monad m, GTraversable ctx a)
   => proxy ctx
-  -> (forall d . GTraversable ctx d => d -> m d)
+  -> (forall d . (GTraversable ctx d, ctx d) => proxy ctx -> d -> m d)
   -> a -> m a
-gmapM ctx f = unwrapMonad . gtraverse ctx (WrapMonad . f)
+gmapM ctx f = unwrapMonad . gtraverse ctx (\ctx -> WrapMonad . f ctx)
 
 gfoldMap
   :: (Monoid r, GTraversable ctx a)
   => proxy ctx
-  -> (forall d . GTraversable ctx d => d -> r)
+  -> (forall d . (GTraversable ctx d, ctx d) => proxy ctx -> d -> r)
   -> a -> r
-gfoldMap ctx f = getConstant . gtraverse ctx (Constant . f)
+gfoldMap ctx f = getConstant . gtraverse ctx (\ctx -> Constant . f ctx)
 
 gfoldr
   :: GTraversable ctx a
   => proxy ctx
-  -> (forall d . (GTraversable ctx d) => d -> r -> r)
-  ->                                     r -> a -> r
-gfoldr ctx f z t = appEndo (gfoldMap ctx (Endo . f) t) z
+  -> (forall d . (GTraversable ctx d, ctx d) => proxy ctx -> d -> r -> r)
+  -> r -> a -> r
+gfoldr ctx f z t = appEndo (gfoldMap ctx (\ctx -> Endo . f ctx) t) z
 
 gfoldl'
   :: GTraversable ctx a
   => proxy ctx
-  -> (forall d . (GTraversable ctx d) => r -> d -> r)
-  ->                                     r -> a -> r
+  -> (forall d . (GTraversable ctx d, ctx d) => proxy ctx -> r -> d -> r)
+  -> r -> a -> r
 gfoldl' ctx f z0 xs = gfoldr ctx f' id xs z0
-  where f' x k z = k $! f z x
+  where f' ctx x k z = k $! f ctx z x
