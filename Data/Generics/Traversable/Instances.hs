de04a47129452ef31671d0ae352de1af8dedd8ad
Roman Cheplyaka <roma@ro-che.info>
2014-11-21 11:20:28 +0200

    Return the accidentally lost instances

--- Data/Generics/Traversable/Instances.hs
+++ Data/Generics/Traversable/Instances.hs
@@ -18,13 +18,10 @@ instance GTraversable c (Ratio n)
 instance GTraversable c Char
 instance GTraversable c Ordering
 
---deriveGTraversable ''Maybe
-instance c_0 a_1 => Data.Generics.Traversable.Core.GTraversable c_0
-                                                                (Maybe a_1)
-    where gtraverse = \f_2 x_3 -> case x_3 of
-                                                                     Nothing -> Control.Applicative.pure Nothing
-                                                                     Just arg_4 -> (Control.Applicative.<*>) (Control.Applicative.pure Just) (f_2 arg_4)
-
+deriveGTraversable ''Maybe
+deriveGTraversable ''Either
+deriveGTraversable ''(,)
+deriveGTraversable ''(,,)
 
 -- Uniform instance for lists
 instance c a => GTraversable c [a] where
