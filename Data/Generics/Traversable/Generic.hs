eafdd968975633501608c8dce4db85e97e58fa66
Isaac van Bakel <isaac.vanbakel@habito.com>
2019-09-02 13:30:27 +0100

    Moves Rec1 instance behind version gate, adds type error message

--- Data/Generics/Traversable/Generic.hs
+++ Data/Generics/Traversable/Generic.hs
@@ -1,11 +1,23 @@
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+
+# if MIN_VERSION_GLASGOW_HASKELL(8, 6, 1, 0)
 {-# LANGUAGE QuantifiedConstraints #-}
+# else
+{-# LANGUAGE DataKinds #-}
+# endif
+
 {-# OPTIONS_GHC -Wno-orphans #-}
 module Data.Generics.Traversable.Generic where
 
 import Data.Generics.Traversable
 import GHC.Generics
+
+# if MIN_VERSION_GLASGOW_HASKELL(8, 6, 1, 0)
+# else
+import GHC.TypeLits (TypeError, ErrorMessage (..))
+# endif
 
 class GTraversable' c (f :: * -> *) where
   gtraverse' :: Applicative g => (forall d. c d => d -> g d) -> (forall p. f p -> g (f p))
@@ -19,8 +31,20 @@ instance GTraversable' c V1 where
 instance GTraversable' c Par1 where
   gtraverse' _f par1 = pure par1
 
+# if MIN_VERSION_GLASGOW_HASKELL(8, 6, 1, 0)
 instance (forall p. GTraversable c (f p)) => GTraversable' c (Rec1 f) where
   gtraverse' f (Rec1 recur) = Rec1 <$> gtraverse @c f recur
+# else
+instance
+    ( TypeError
+      ( 'Text "No generic instance can be derived for " ':$$: 'ShowType f ':$$:
+        'Text " for this GHC version - upgrade to at least 8.6.1 to be" ':$$:
+        'Text " able to use `GTraversable` from `Generic` here"
+      )
+    )
+  => GTraversable' c (Rec1 f) where
+  gtraverse' _ _ = error "Cannot implement `gtraverse'` for this type"
+# endif
 
 instance (c con) => GTraversable' c (K1 i con) where
   gtraverse' f (K1 con) = K1 <$> f con
